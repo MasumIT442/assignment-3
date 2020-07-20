63bf2a947b3c357c6a3eebd94eef996cb01af96c
MasumIT442 <masumit442@gmail.com>
2020-07-20 14:03:56 +0600

    t

--- assignment.js
+++ assignment.js
@@ -1,6 +1,16 @@
+// feetToMile Start
+
 function feetToMile(feet){
     var mile = feet/5280;
     return mile;
     }
 var jhankarSir = feetToMile(52800);
 console.log(jhankarSir)
+
+// feetToMile End 
+
+// woodCalculation Start 
+I am new in java. Not clear everything, trying hard to understand. So,
+// u r requested to pray for menubar. 
+
+}
